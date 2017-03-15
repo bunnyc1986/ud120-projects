90146d83907721622b210e6c1d511bd6ed6e298d
Yang Fan <bunnyc1986@gmail.com>
2017-03-14 20:25:47 -0700

    naive bayes.

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -21,12 +21,23 @@ from email_preprocess import preprocess
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
-
-
-
 #########################################################
 ### your code goes here ###
+from sklearn.naive_bayes import GaussianNB
+clf = GaussianNB()
 
+# Train
+t0 = time()
+clf.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
+
+# Predict
+t1 = time()
+print "predict labels:", clf.predict(features_test)
+print "training time:", round(time()-t1, 3), "s"
+
+accuracy = clf.score(features_test, labels_test)
+print(accuracy)
 
 #########################################################
 
